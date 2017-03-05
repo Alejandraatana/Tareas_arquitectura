9bf70f48f835e1774fed0bffccae24db291d914d
Alejandra <alejandra.atana.rodriguez@gmail.com>
2017-03-05 13:16:05 -0600

    Comparacion de exponentes

--- std_logic/sumfloat.vhdl
+++ std_logic/sumfloat.vhdl
@@ -18,8 +18,11 @@ signal mantizax: std_logic_vector(0 downto 23);
 signal   signoy: std_logic;
 signal     expy: std_logic_vector(0 downto 7);
 signal mantizay: std_logic_vector(0 downto 23);
-
 signal   resul: std_logic_vector(0 downto 31)="00000000000000000000000000000000";
+signal subaux1: std_logic_vector(0 downto 8);
+signal subaux2: std_logic_vector(0 downto 8);
+
+
 begin
 	process(x,y)
 	begin
@@ -29,6 +32,14 @@ begin
 		expoy<=y(1 downto 8);
 		mantizax<=('1' & x(9 downto 31));
 		mantizay<=('1' & x(9 downto 31));
+	end process;
+
+	process(expox,expoy)
+	begin
+		subaux1=expx-expy;
+		subaux2=expy-expx;
+		if subaux1(0)='1' then
+		elsif subaux2(0)='1' then
 	end process;
 	
 end architecture beh;	
